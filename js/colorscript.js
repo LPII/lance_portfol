1b9af22a42fa74ac5f7d38f7dc40a79dca1df6e8
Lporter <Lanceportergd@gmail.com>
2015-02-13 18:50:30 -0500

    commit beofre remote push

--- js/colorscript.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-	colors();
-});
-
-function colors() {
-	var color = '#'+Math.floor(Math.random()*16777215).toString(16);
-	$('html').animate({backgroundColor: color}, {duration: 15000, complete: colors});
-    //console.log(color);
-}
